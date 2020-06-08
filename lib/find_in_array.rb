a39cf07a074e5aae5523d3c2df5180cc431ca22a
scrappy-strategy-4846 <tomlai8882000@hotmail.com>
2020-06-08 21:45:06 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,8 +1,8 @@
 def find_element_index(array, value_to_find)
   count = 0 
-  found_value_index = nil 
   while count < array.length do
     if array[count] == value_to_find
+      return count
       found_value_index = count
     end 
     
